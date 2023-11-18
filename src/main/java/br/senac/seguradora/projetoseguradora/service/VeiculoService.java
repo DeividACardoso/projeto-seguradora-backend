06d36dadd12af10914634f28bb014a6217d2b543
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-11-18 07:40:33 -0300

    Ajustes

--- src/main/java/br/senac/seguradora/projetoseguradora/service/VeiculoService.java
+++ src/main/java/br/senac/seguradora/projetoseguradora/service/VeiculoService.java
@@ -2,33 +2,38 @@ package br.senac.seguradora.projetoseguradora.service;
 
 import java.util.List;
 
-import br.senac.seguradora.projetoseguradora.model.entidade.Veiculo;
+import org.springframework.stereotype.Service;
 
+import br.senac.seguradora.projetoseguradora.model.entidade.Veiculo;
+import br.senac.seguradora.projetoseguradora.model.seletor.VeiculoSeletor;
+import jakarta.transaction.Transactional;
+
+@Service
 public class VeiculoService {
 
+	@Transactional
 	public List<Veiculo> listarTodos() {
-		
 		return null;
 	}
 
 	public Veiculo consultarPorId(long longValue) {
-		
 		return null;
 	}
 
 	public Veiculo inserir(Veiculo novoVeiculo) {
-		
 		return null;
 	}
 
 	public Object atualizar(Veiculo veiculoParaAtualizar) {
-		
 		return null;
 	}
 
 	public boolean excluir(Integer id) {
-		
 		return false;
+	}
+
+	public List<Veiculo> listarComSeletor(VeiculoSeletor seletor) {
+		return null;
 	}
 
 }
